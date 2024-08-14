5811bae0bf94272fae602beaf40be166893a4492
Aferist163 <aferist.game.163@gmail.com>
2024-08-14 16:30:12 +0200

    innitial

--- src/Js/App.js
+++ src/Js/App.js
@@ -35,8 +35,8 @@ import Play from '../IMG/All_Logo/Play.png';
 import Octo from '../IMG/All_Logo/Octo.png';
 import invite from '../IMG/All_Logo/Invite_png.png';
 import Join from '../IMG/All_Logo/Join.png';
-import GNft from '../IMG/Nft_ref/GNFT.gif';
-// import Nft from '../IMG/Nft_ref/GNFT.gif';
+// import GNft from '../IMG/Nft_ref/GNFT.gif';
+import Nft from '../IMG/Nft_ref/NFT.MP4';
 import Checknft from '../IMG/Nft_ref_check/chech.png';
 import ChecknftDone from '../IMG/Nft_ref_check_done/Done_ref.png';
 import NFTm from '../IMG/All_Logo/NFTmint.png';
@@ -516,10 +516,10 @@ const handleCheckReferrals = () => {
             </div>
           </div>
           <div className='nft-image'>
-            <img src={GNft} alt='OCTIES NFT' />
-            {/* <video id='NftVd' autoplay loop muted>
-              <source src={VNft} type="video/mp4"/>
-            </video> */}
+            {/* <img src={GNft} alt='OCTIES NFT' /> */}
+            <video id='NftVd' autoplay loop muted>
+              <source src={Nft} type="video/mp4"/>
+            </video> 
           </div>
         </div>}
 
