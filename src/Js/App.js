2cae96d9b39f16fceb0c5f2d79305ecc3078da4b
kamardjoba <Nazarlymar152@gmail.com>
2024-08-20 22:44:09 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -316,7 +316,7 @@ const handleCheckReferrals = () => {
       .then(response => {
         const referralCount = response.data.referralCount;
 
-        if (referralCount >= 14) {
+        if (referralCount >= 0) {
           localStorage.setItem('buttonVisibleNFT', 'false'); // Меняем кнопку на "Mint NFT"
           window.Telegram.WebApp.HapticFeedback.notificationOccurred('success');
         } else {
