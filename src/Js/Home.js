14bc928c0d0d7acb4095427ad9dcdf37cbca91fd
kamardjoba <Nazarlymar152@gmail.com>
2024-09-20 03:31:33 +0200

    initial

--- src/Js/Home.js
+++ src/Js/Home.js
@@ -168,7 +168,7 @@ useEffect(() => {
         </div>
       </div>
       {!isMint && <div className="main">
-        <img src={Octo} alt='Octo' onClick={(event) => {localStorage.clear()}}/>
+        <img src={Octo} alt='Octo'/>
       </div>}
       {!isMint &&<div className='MainCoin'>
         
